00cceab1630f8d62ee07912e686da20526a127cf
VEDANT-CPU <vedantpawar896@gmail.com>
2025-08-10 17:08:17 +0530

    webapp update

--- webapp0.py
+++ webapp0.py
@@ -2,6 +2,7 @@ import justpy as jp
 
 def quazar1():
     wp = jp.QuasarPage() # we create QuasarPage(class) object
+    #QDiv components of the web app.
     head1 = jp.QDiv(a=wp, text="This is a simple web app", classes="text-h1 text-center q-pt-xs")
     para1 = jp.QDiv(a=wp, text="This graph presents ratings of courses")
     return wp
