114e2a947ad62827d116e1fa70e8ac739d671ec3
VEDANT-CPU <vedantpawar896@gmail.com>
2025-08-10 16:19:38 +0530

    Added QDiv components

--- webapp0.py
+++ webapp0.py
@@ -1,4 +1,10 @@
 import justpy as jp
 
 def quazar1():
-    wp = jp.QuasarPage
+    wp = jp.QuasarPage() # we create QuasarPage(class) object
+    head1 = jp.QDiv(a=wp, text="This is a simple web app", classes="text-h1 text-center q-pt-xs")
+    para1 = jp.QDiv(a=wp, text="This graph presents ratings of courses")
+    return wp
+
+#Now, we need a function call to create and access the quasar page.
+jp.justpy(quazar1) # Expects a function which returns a qausar page as its input.
